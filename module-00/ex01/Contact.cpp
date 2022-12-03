33b41a05dbd6ee207726963283f81398ec241e1c
Brahim Berkasse <berkassebrahim@gmail.com>
2022-12-04 00:59:16 +0100

    🍞 form fields to not pass 10 chars !

--- module-00/ex01/Contact.cpp
+++ module-00/ex01/Contact.cpp
@@ -3,6 +3,12 @@
 #include <iomanip>
 #include "Contact.hpp"
 
+std::string form_field(std::string input){
+  if (input.length() > 10)
+    return input.substr(0, 9) + '.';
+  return input;
+}
+
 void Contact::add_contact(){
   std::cout << "First Name : ";
   std::cin >> this->first_name;
@@ -19,8 +25,8 @@ void Contact::add_contact(){
 
 void Contact::toString(int index) {
   std::cout << std::right << std::setw(10) << index << " |";
-  std::cout << std::right << std::setw(10) << this->first_name << " |";
-  std::cout << std::right << std::setw(10) << this->last_name << " |";
-  std::cout << std::right << std::setw(10) << this->nickname << " |";
+  std::cout << std::right << std::setw(10) << form_field(this->first_name) << " |";
+  std::cout << std::right << std::setw(10) << form_field(this->last_name) << " |";
+  std::cout << std::right << std::setw(10) << form_field(this->nickname) << " |";
   std::cout << "\n";
 }
